fe7113047356161c748be2f5909dc8c2eccec1e9
wonderboy <wonderboy39@outlook.com>
2017-11-05 22:47:51 +0900

    lec02 : linear regression의 첫 단계인 cost function, minimizing cost에 대한 설명

---
+++ img/change_screenshot_dir_MD.sh
@@ -0,0 +1,5 @@
+#defaults write com.apple.screencapture location ~/workspace/scrapper/model1/MD/img/
+#defaults write com.apple.screencapture location ~/workspace/scrapper/python/ml_basic/img/
+
+defaults write com.apple.screencapture location /Users/soon/workspace/python/ml_basic/img
+
